a8bb093b9df81a9624c014173feb9808d2b53010
Paulo Marcos <132699727+Paulo-Marcos1@users.noreply.github.com>
2024-08-18 20:06:45 -0300

    jogo_numero_aleatorio.py

--- exercicios/jogo_numero_aleatorio.py
+++ exercicios/jogo_numero_aleatorio.py
@@ -5,7 +5,7 @@ while True:
     tentativa = int(input("Adivinhe o numero de 1 a 100: \n"))
     contador = contador + 1
     if contador == 8:
-        print("Você perdeu")
+        print("Você perdeu o numero era:",num_secreto)
         break
     if tentativa > num_secreto:
         print("Tente um numero menor")
