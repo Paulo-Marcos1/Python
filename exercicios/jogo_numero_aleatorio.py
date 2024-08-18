cb4a204d5c3cabbfe1fc681017451df034724359
Paulo Marcos <132699727+Paulo-Marcos1@users.noreply.github.com>
2024-08-18 20:01:32 -0300

    Add files via upload

---
+++ exercicios/jogo_numero_aleatorio.py
@@ -0,0 +1,17 @@
+import random
+num_secreto= random.randint(1,100)
+contador = 0
+while True:
+    tentativa = int(input("Adivinhe o numero de 1 a 100: \n"))
+    contador = contador + 1
+    if contador == 8:
+        print("Você perdeu")
+        break
+    if tentativa > num_secreto:
+        print("Tente um numero menor")
+    elif tentativa < num_secreto:
+        print("Tente um numero maior")
+    else:
+        print("Você acertou o numero")
+        break
+    print("Você tentou ",contador,(" vezes!"))
