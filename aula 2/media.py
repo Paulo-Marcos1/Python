3bde5257aa18acd4d2de24cbe8e4d559f9edba5c
Paulo Marcos <132699727+Paulo-Marcos1@users.noreply.github.com>
2024-09-26 14:07:22 -0300

    Create media.py

---
+++ aula 2/media.py
@@ -0,0 +1,5 @@
+nome = str(input("Digite seu nome:"))
+nota1 = int(input("Digite a primeira nota:"))
+nota2 = int(input("digite a segunda nota:"))
+media = (nota1+nota2)/2
+print(f"{nome} sua media foi {media}")
