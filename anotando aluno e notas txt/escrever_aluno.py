ce31d5ac7c7885a082e91c9392258426cc9619db
Paulo Marcos <132699727+Paulo-Marcos1@users.noreply.github.com>
2025-03-27 23:37:08 -0300

    Create escrever_aluno.py

---
+++ anotando aluno e notas txt/escrever_aluno.py
@@ -0,0 +1,12 @@
+def escrever_aluno():
+   nome = str(input("Digite o nome do Aluno: "))
+   n1 = float(input("Digite a primeira nota: "))
+   n2 = float(input("Digite a segunda nota: "))
+   media = (n1 + n2)/2
+   dados = f"Aluno: {nome} \n1ª Prova:{n1} \n2ª Prova:{n2} \nMedia:{media}\n\n"
+
+   caminho = open("Alunos.txt", "a", encoding="utf-8")
+   caminho.writelines(dados)
+   caminho.close()
+
+escrever_aluno()
